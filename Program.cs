f64397331224a665302ed41e1a713d195edb708d
unknown <xx06102002@gmail.com>
2022-11-20 16:52:17 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Programa que lea una serie de números (leer hasta que llegue el cero, por ejemplo) y que calcule su media aritmética 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double num = 1; double media = 0; int i = 0;
+        while(num != 0)
+        {
+            Console.WriteLine("Digite un número");
+            num = double.Parse(Console.ReadLine());
+
+            i++;
+            media = media + num;
+
+        }
+        Console.WriteLine("Finalizado");
+        Console.WriteLine("La media aritmética de los números ingresados es: "+media / i);
+           
+    }
+}
